670fb7f4c9d9ca74538c0d40bc42f77a35712290
Mojz3szP <paradowski1200@gmail.com>
2024-01-14 14:37:20 +0100

    refactoring structure of code

--- zadanie.js
+++ zadanie.js
@@ -25,27 +25,30 @@
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
   };
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+      done: false,
+    });
+    render();
+  };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTasks").value.trim();
+    console.log(newTaskContent);
+    if (newTaskContent === "") {
+      return;
+    }
+    addNewTask(newTaskContent);
+  };
+
   const init = () => {
     render();
 
     const form = document.querySelector(".js-form");
 
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      const newTaskContent = document
-        .querySelector(".js-newTasks")
-        .value.trim();
-      console.log(newTaskContent);
-      if (newTaskContent === "") {
-        return;
-      }
-
-      tasks.push({
-        content: newTaskContent,
-      });
-      render();
-    });
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
